9dfe60c31c18297aa6caeb812a6bb2a8d862113e
Denick <1049485+apie@users.noreply.github.com>
2019-12-02 09:52:42 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -6,9 +6,8 @@ def read(fname):
 
 setup(
     name='django-persistent-messages',
-    version='0.1',
+    version='0.1.2',
     description='A Django app for unified and persistent user messages/notifications, built on top of Django\'s messages framework',
-    long_description=read('README.md'),
     author='philomat',
     license='BSD',
     url='https://github.com/Crop-R/django-persistent-messages',
@@ -24,6 +23,12 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Framework :: Django',
     ],
     zip_safe=False,
